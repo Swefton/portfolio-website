61f3efbea66c4e49ae7005978272b6016e5979f1
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-05-07 17:01:11 +0530

    code block component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Navbar from "./navbar/navbar";
+import CodeBlock from "./page/CodeBlock";
+
 
 function App() {
+
+  const code = `
+  function helloWorld() {
+    console.log('Hello, world!');
+  }
+  helloWorld();
+`;
+
   return (
     <div className="App">
       <Navbar />
+      <CodeBlock code={code} />
+
     </div>
   );
 }
---
+++ src/page/CodeBlock.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './CodeBlock.css';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism'; // Choose a theme
+
+
+function CodeBlock({ code }) {
+  // Split the code into lines
+  const lines = code.split('\n');
+
+  return (
+    <div className="code-block">
+        <div className="code-content">
+          {lines.map((line, index) => (
+            <div key={index} className="code-line">
+              <span className="line-number">{index + 1}</span>
+              <SyntaxHighlighter language="javascript" style={atomDark}>
+                {line}
+              </SyntaxHighlighter>
+              
+            </div>
+          ))}
+        </div>
+    </div>
+  );
+}
+
+export default CodeBlock;
