1edefb63a9bb9853f165bca1409850b933490fa6
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-05-09 11:27:09 +0530

    Typing Animation

--- src/page/CodeBlock.js
+++ src/page/CodeBlock.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/CodeBlock.css';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 function CodeBlock({ code }) {
+  const [displayedCode, setDisplayedCode] = useState('');
+  const [animationState, setAnimationState] = useState(
+    localStorage.getItem(`code-block-${code}`) || 0
+  );
+
+  useEffect(() => {
+    const codeLength = code.length;
+    let currentIndex = animationState;
+
+    const typingInterval = setInterval(() => {
+      setDisplayedCode(code.substring(0, currentIndex));
+      currentIndex++;
+      if (currentIndex > codeLength) {
+        clearInterval(typingInterval);
+      } else {
+        setAnimationState(currentIndex);
+        localStorage.setItem(`code-block-${code}`, currentIndex);
+      }
+    }, 15); // Adjust the typing speed here (milliseconds)
+
+    return () => clearInterval(typingInterval);
+  }, [code, animationState]);
+
+  // Clear localStorage on page refresh and tab close
+  useEffect(() => {
+    const handleRefresh = () => {
+      localStorage.removeItem(`code-block-${code}`);
+    };
+
+    window.onbeforeunload = handleRefresh;
+    window.onunload = handleRefresh;
+
+    return () => {
+      window.onbeforeunload = null;
+      window.onunload = null;
+    };
+  }, [code]);
 
   return (
     <div className="code-block">
       <SyntaxHighlighter language="python" style={atomDark} showLineNumbers>
-        {code}
+        {displayedCode}
       </SyntaxHighlighter>
     </div>
   );
--- src/page/resume.js
+++ src/page/resume.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import '../styles/Resume.css';
 
 function Resume() {
@@ -7,6 +7,7 @@ function Resume() {
             <iframe
                 class="pdf" 
                 src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210101201653/PDF.pdf#toolbar=0&#FitH"
+                title='resume'
                 >
             </iframe>
         </div>
