a56eb598a925937d688f82920e8fe355096a9620
Swefton <amrit.madhav.srivastava@gmail.com>
2025-04-09 11:41:11 -0400

    some quick meta data fixes and eye candy
    changed blurry favicon, added new resume, added support for some apple standard of favicon (don't fully understand why it is important but it is added) and also changed open graph meta tags, these need further updates and then a recache

--- src/page/resume.js
+++ src/page/resume.js
@@ -4,8 +4,7 @@ import '../styles/Resume.css';
 function Resume() {
   return (
     <div className='pdf'>
-        <object data="/Amrit Srivastava Resume.pdf#toolbar=0" type="application/pdf" width="100%" height="100%">
-            <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+        <object data="/Amrit Resume.pdf#toolbar=0" type="application/pdf" width="100%" height="100%">
         </object>
     </div>
   );
