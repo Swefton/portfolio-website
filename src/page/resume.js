66df9c8942156c2a6d36638c8bb9e080cfa1e58f
Swefton <amrit.madhav.srivastava@gmail.com>
2025-04-09 15:28:27 -0400

    updated resume tab
    resume tab now has one scroll bar and uses the react-pdf library to look more visually appealing

--- src/page/resume.js
+++ src/page/resume.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
 import '../styles/Resume.css';
 
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+
 function Resume() {
+  const containerRef = useRef(null);
+  const [containerWidth, setContainerWidth] = useState(0);
+
+  // ResizeObserver to update containerWidth
+  useEffect(() => {
+    const observer = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        setContainerWidth(entry.contentRect.width);
+      }
+    });
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <div className='pdf'>
-        <object data="/Amrit Resume.pdf#toolbar=0" type="application/pdf" width="100%" height="100%">
-        </object>
+    <div className="pdf-wrapper">
+      <div className="pdf-container" ref={containerRef}>
+        <Document file="/Amrit Resume.pdf" loading="Loading resume...">
+          <Page pageNumber={1} width={containerWidth * 1} />
+        </Document>
+      </div>
     </div>
   );
 }
