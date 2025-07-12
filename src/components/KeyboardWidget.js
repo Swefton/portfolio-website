a8bad7c6e6b37d31417cfb4dbbafd19a734821c4
Amrit <amrit.madhav.srivastava@gmail.com>
2025-07-11 22:18:47 -0400

    some basic styling
    globals.css needs work, better consistency for the aesthetic

--- src/components/KeyboardWidget.js
+++ src/components/KeyboardWidget.js
@@ -4,9 +4,65 @@
 import { useState, useRef, forwardRef, useImperativeHandle } from 'react';
 import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
+import './keyboard.css';
 
 const KeyboardWidget = forwardRef((props, ref) => {
   const keyboardRef = useRef();
+  const [layoutName, setLayoutName] = useState("default");
+
+  const keyboardOptions = {
+    layoutName,
+    physicalKeyboardHighlight: true,
+    theme: "simple-keyboard hg-theme-default hg-layout-default",
+    syncInstanceInputs: true,
+    mergeDisplay: true,
+    debug: false,
+    layout: {
+      default: [
+        "` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
+        "{tab} q w e r t y u i o p [ ] \\",
+        "{capslock} a s d f g h j k l ; ' {enter}",
+        "{shiftleft} z x c v b n m , . / {shiftright}",
+        "{controlleft} {altleft} {metaleft} {space} {altright} {controlright}"
+      ],
+      shift: [
+        "~ ! @ # $ % ^ & * ( ) _ + {backspace}",
+        "{tab} Q W E R T Y U I O P { } |",
+        '{capslock} A S D F G H J K L : " {enter}',
+        "{shiftleft} Z X C V B N M < > ? {shiftright}",
+        "{controlleft} {altleft} {metaleft} {space} {altright} {controlright}"
+      ]
+    },
+    display: {
+      "{backspace}": "⌫",
+      "{tab}": "⇥",
+      "{enter}": "↵",
+      "{capslock}": "⇪",
+      "{shiftleft}": "⇧",
+      "{shiftright}": "⇧",
+      "{controlleft}": "⌃",
+      "{controlright}": "⌃",
+      "{altleft}": "⌥",
+      "{altright}": "⌥",
+      "{metaleft}": "⌘",
+      "{metaright}": "⌘",
+      "{space}": "␣"
+    },
+    onKeyPress: (button) => {
+      if (
+        button === "{shift}" ||
+        button === "{shiftleft}" ||
+        button === "{shiftright}" ||
+        button === "{capslock}"
+      ) {
+        handleShift();
+      }
+    }
+  };
+
+  const handleShift = () => {
+    setLayoutName(prev => (prev === "default" ? "shift" : "default"));
+  };
 
   useImperativeHandle(ref, () => ({
     setInputValue: (newInput) => {
@@ -17,19 +73,14 @@ const KeyboardWidget = forwardRef((props, ref) => {
     },
     getButtonElement: (button) => {
       return keyboardRef.current.getButtonElement(button);
-    },
-    getCurrentInput: () => {
-      return input;
-    },
+    }
   }));
 
   return (
-      <Keyboard
-        keyboardRef={r => (keyboardRef.current = r)}
-        // onChange={handleKeyboardChange}
-        // onKeyPress={handleKeyPress}
-        physicalKeyboardHighlight={true}
-      />
+    <Keyboard
+      keyboardRef={r => (keyboardRef.current = r)}
+      {...keyboardOptions}
+    />
   );
 });
 
