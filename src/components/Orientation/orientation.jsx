e049f014c3eada5ac378fa8bf8a99622911275a2
Swefton <amrit.madhav.srivastava@gmail.com>
2025-12-15 12:46:09 -0500

    fixed artifact on wrong orientation

--- src/components/Orientation/orientation.jsx
+++ src/components/Orientation/orientation.jsx
@@ -42,12 +42,19 @@ const OrientationLock = ({ children }) => {
               <div className="phone-screen"></div>
             </div>
             <div className="rotation-arrow">
-              <svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <path d="M21 2l-2 2m-7.61 7.61a3 3 0 11-4.24 4.24 3 3 0 014.24-4.24z"/>
-                <path d="M13 2l6 6-6 6"/>
-                <path d="M21 8.5l-7.5 7.5"/>
-              </svg>
-            </div>
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6 7L7 6L4.70711 3.70711L5.19868 3.21553C5.97697 2.43724 7.03256 2 8.13323 2C11.361 2 14 4.68015 14 7.93274C14 11.2589 11.3013 14 8 14C6.46292 14 4.92913 13.4144 3.75736 12.2426L2.34315 13.6569C3.90505 15.2188 5.95417 16 8 16C12.4307 16 16 12.3385 16 7.93274C16 3.60052 12.4903 0 8.13323 0C6.50213 0 4.93783 0.647954 3.78447 1.80132L3.29289 2.29289L1 0L0 1V7H6Z"
+                fill="currentColor"
+              />
+            </svg>
+          </div>
           </div>
           <h2 className="orientation-title">Please Rotate Your Device</h2>
           <p className="orientation-message">
