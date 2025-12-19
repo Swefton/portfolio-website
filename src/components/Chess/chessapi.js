026138d8cae09768fa66a2285a3cc0acb554f6c1
Swefton <amrit.madhav.srivastava@gmail.com>
2025-12-19 15:19:01 -0500

    chess board setup and info from chesscom api

--- src/components/Chess/chessapi.js
+++ src/components/Chess/chessapi.js
@@ -1,18 +1,42 @@
 // testApi.js
+import { Chess } from 'chess.js';
 
 const url = "https://api.chess.com/pub/player/sweftonxd/games/live/180/0";
 
 async function fetchGame() {
+  
   try {
     const response = await fetch(url);
-
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
 
     const data = await response.json();
-    console.log(typeof data)
-    console.log("API response:", data.games[data.games.length - 1]);
+    const pgn = data.games[data.games.length - 1].pgn;
+
+    const chess = new Chess();
+    chess.loadPgn(pgn);
+
+    const moves = chess.history();
+    let moveIndex = 0;
+
+    let playboard = new Chess();
+
+    function stepForward() {
+      if (moveIndex >= moves.length) return;
+
+      playboard.move(moves[moveIndex]);
+      moveIndex++;
+
+      console.log(playboard.ascii());
+    }
+
+    console.log(chess.getHeaders());
+
+    for (let i = 0; i < 10; ++i) {
+      stepForward();
+    }
+
   } catch (error) {
     console.error("Error fetching API:", error);
   }
